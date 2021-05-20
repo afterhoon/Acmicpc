bd1457d26c0db7bb46226088cacff9ed129ebff2
= <=>
2021-05-20 21:09:56 +0900

    14888 수정

--- 14888.py
+++ 14888.py
@@ -2,31 +2,27 @@
 ## https://www.acmicpc.net/problem/14888
 import sys
 
-def operation(i, res, op):
+def operation(i, res, add, sub, mul, div):
     global max_, min_
     if i == N:
         max_ = max(res, max_)
         min_ = min(res, min_)
-
+        return
     else:
-        if op[0]:
-            op[0] = op[0] - 1
-            operation(i+1, res+nums[i], op)
-        if op[1]:
-            op[1] = op[1] - 1
-            operation(i+1, res-nums[i], op)
-        if op[2]:
-            op[2] = op[2] - 1
-            operation(i+1, res*nums[i], op)
-        if op[3]:
-            op[3] = op[3] - 1
-            operation(i+1, int(res/nums[i]), op)
+        if add:
+            operation(i+1, res+nums[i], add-1, sub, mul, div)
+        if sub:
+            operation(i+1, res-nums[i], add, sub-1, mul, div)
+        if mul:
+            operation(i+1, res*nums[i], add, sub, mul-1, div)
+        if div:
+            operation(i+1, int(res/nums[i]), add, sub, mul, div-1)
 
 N = int(input())
 nums = list(map(int, input().split()))
-op = list(map(int, input().split()))
+add, sub, mul, div = map(int, input().split())
 min_, max_ = 1000000000, -1000000000
 
-operation(1, nums[0], op)
+operation(1, nums[0], add, sub, mul, div)
 print(max_)
 print(min_)
