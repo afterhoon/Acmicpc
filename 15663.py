5259af1c8b0e85d7d2192a0d9a981a75ae92b5b3
afterhoon <hooni0517@gmail.com>
2021-07-23 21:25:06 +0900

    15663

--- 15663.py
+++ 15663.py
@@ -1,14 +1,16 @@
 ## N과 M(9)
 ## https://www.acmicpc.net/problem/15663
-## 틀림
+
 from itertools import permutations
 
 n, m = map(int, input().split())
-arr = list(map(str, input().split()))
+arr = list(map(int, input().split()))
+# 입력받은 숫자를 permutations() 함수로 수열을 만들고 per에 입력한 뒤
+# set를 한번 거쳐서 중복을 제거하고 sort()로 정렬한다
 per = list(set(permutations(arr, m)))
 per.sort()
 
 for i in range(len(per)):
-    for j in range(m):
+    for j in range(len(per[i])):
         print(per[i][j], end=' ')
     print()
