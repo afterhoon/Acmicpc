b03978a3d9319b9a69d66967781217a1e38b0065
afterhoon <hooni0517@gmail.com>
2021-10-16 22:04:27 +0900

    11170

--- 11170.py
+++ 11170.py
@@ -4,7 +4,9 @@
 t = int(input())
 
 for _ in range(t):
+    cnt = 0
     n, m = map(int, input().split())
     for i in range(n,m+1):
-        num = str(i)
-        
+        # 숫자를 string으로 변환한 뒤 0의 개수를 카운트한다
+        cnt += str(i).count("0")
+    print(cnt)
