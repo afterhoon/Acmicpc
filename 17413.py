aadaf1baf4b695656cd02e82bf6d93da2031f7e9
= <hooni0517@gmail.com>
2021-10-18 13:06:23 +0900

    17413

--- 17413.py
+++ 17413.py
@@ -1,9 +1,25 @@
 ## 단어 뒤집기 2
 ## https://www.acmicpc.net/problem/17413
-## x
-S = list(map(str, input().split()))
 
-for s in S:
-    if s[0] == '<' and s[len(s)-1] == '>':
-        print(s, end="")
-    print(s[::-1], end=" ")
+s = input()
+buffer = "" # 특정 문자가 나올때까지 문자열을 받아들인다
+mode = 0    # 0: INSERT , 1: READ
+for i in range(len(s)):
+    if mode == 0:
+        if s[i] == " ":
+            print(buffer[::-1], end=" ")
+            buffer = ""
+            continue
+        elif s[i] == "<":
+            print(buffer[::-1], end="")
+            print("<", end="")
+            buffer = ""
+            mode = 1 # 괄호 안의 문자열은 그대로 출력해야하므로 
+                     # mode = 1(READ) 로 변경
+            continue
+        buffer += s[i]
+    elif mode == 1: # 괄호 안이므로 그대로 출력한다
+        print(s[i], end="")
+        if s[i] == ">": # 괄호가 끝나면 mode를 0(INSERT)로 바꾼다
+            mode = 0
+print(buffer[::-1]) # 버퍼에 남은 문자열을 처리한다
