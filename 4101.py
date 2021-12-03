b437139984095881182a3cc1de1ca167d8953a20
afterhoon <hooni0517@gmail.com>
2021-12-03 13:12:59 +0900

    4101

--- 4101.py
+++ 4101.py
@@ -5,4 +5,4 @@ while True:
     a, b = map(int, input().split())
     if a == 0 and b == 0:
         break
-    print("YES" if a > b else "NO") #a가 b보다 클때만 YES 출력
+    print("Yes" if a > b else "No") #a가 b보다 클때만 YES 출력
