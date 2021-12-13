5da9cc7dfb9ed5b3d91bd6eb8cb37b1770d277ac
afterhoon <hooni0517@gmail.com>
2021-12-13 22:41:09 +0900

    2010

--- 2010.py
+++ 2010.py
@@ -1,9 +1,13 @@
 ## 플러그
 ## https://www.acmicpc.net/problem/2010
 
+import sys
+input = sys.stdin.readline
 n = int(input())
-plug = []
+sum = 0
+cnt = 0
 for _ in range(n):
-    plug.append(int(input()))
+    sum += int(input())
+    cnt += 1
 
-print(sum(plug) - (len(plug) - 1)) # 꽂을 수 있는 플러그의 수는 모든 소켓수 - (멀티탭의 개수 - 1) 이다
+print(sum - (cnt - 1)) # 꽂을 수 있는 플러그의 수는 모든 소켓수 - (멀티탭의 개수 - 1) 이다
