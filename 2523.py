76c920bd27cb89cdb6a056c0c36f34fee7f0a877
afterhoon <hooni0517@gmail.com>
2022-01-06 18:15:20 +0900

    2523

---
+++ 2523.py
@@ -0,0 +1,12 @@
+## 별 찍기 - 13
+## https://www.acmicpc.net/problem/2523
+
+n = int(input())
+for i in range(n):
+    for j in range(i+1):
+        print("*", end="")
+    print()
+for i in range(n-1):
+    for j in range(n-i-1):
+        print("*", end="")
+    print()
