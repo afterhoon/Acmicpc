bfce98366a3b965a614a438ed1650dbda01eee16
afterhoon <hooni0517@gmail.com>
2022-02-13 22:31:22 +0900

    2688

---
+++ 2688.py
@@ -0,0 +1,14 @@
+## 줄어들지 않아
+## https://www.acmicpc.net/problem/2688
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    dp = [1 for i in range(10)]
+
+    for i in range(n-1):
+        for j in range(10):
+            dp[j] = sum(dp[j:])
+            
+    print(sum(dp))
