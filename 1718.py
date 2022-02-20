b014fb5af4a50a940bf20861fd499c353a6fe86d
afterhoon <hooni0517@gmail.com>
2022-02-21 00:02:43 +0900

    1718

--- 1718.py
+++ 1718.py
@@ -1,20 +1,16 @@
 ## 암호
 ## https://www.acmicpc.net/problem/1718
 
-alp = [chr(i) for i in range(97, 97+26)]
-print(alp)
-
 plain = list(input())
 crypt = list(input())
 clen = len(crypt)
 ans = []
 i = 0
 for c in plain:
-    tmp = ord(c) - ord(crypt[i])
+    tmp = ord(c) - ord(crypt[i]) - 1
     if tmp < 0:
         tmp = 26 + tmp
     i = (i + 1) % clen
-    if c != ' ':
-        ans.append(chr(tmp + ord('a')))
+    ans.append(' ' if c == ' ' else chr(tmp + ord('a')))
 
-print(ans)
+print("".join(ans))
