cb2c6358c9e7638ea21b639f2be4ec8ee428cec7
afterhoon <hooni0517@gmail.com>
2022-03-31 21:10:28 +0900

    2338

--- 17863.py
+++ 17863.py
@@ -1,8 +1,8 @@
 ## FYI
 ## https://www.acmicpc.net/problem/17863
 
-n = list(input())
-if n[0] == n[1] == n[2] == '5':
-    print("YES")
-else:
-    print("NO")
+    n = list(input())
+    if n[0] == n[1] == n[2] == '5':
+        print("YES")
+    else:
+        print("NO")
